f08b7ac655bb4081705b65a98e278ae41e647df2
Alejandro Zuno Flores <alejandro.zuno@ibm.com>
2021-08-23 13:50:27 -0500

    Using Authentication functions in api

--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,11 +1,21 @@
 # These functions need to be implemented
+import jwt 
+import os
+from authentication import tokenVerification, getRole
+from flask import abort
+
 class Token:
 
-    def generate_token(self, username, password):
-        return 'test'
+    def generate_token(self,user):
+        encoded_jwt = jwt.encode({"role": getRole(user)}, os.environ['JWT_SEED'], algorithm="HS256")
+        return encoded_jwt
 
 
 class Restricted:
 
     def access_data(self, authorization):
-        return 'test'
+        token = str.replace(str(authorization), 'Bearer ', '')
+        if tokenVerification(token):
+            return "You are under protected data"
+        else:
+            return "Access Denied, Invalid Token"
