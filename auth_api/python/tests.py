191ad2b20a61656e1182c5569a5ed62a80bc56ec
Alejandro Zuno Flores <alejandro.zuno@ibm.com>
2021-08-23 13:50:54 -0500

    Adding extra tests

--- auth_api/python/tests.py
+++ auth_api/python/tests.py
@@ -1,6 +1,6 @@
 import unittest
 from methods import Token, Restricted
-
+from werkzeug.exceptions import Forbidden
 
 class TestStringMethods(unittest.TestCase):
 
@@ -9,10 +9,16 @@ class TestStringMethods(unittest.TestCase):
         self.validate = Restricted()
 
     def test_generate_token(self):
-        self.assertEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI', self.convert.generate_token('admin', 'secret'))
+        self.assertEqual('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4ifQ.BmcZ8aB5j8wLSK8CqdDwkGxZfFwM1X1gfAIN7cXOx9w', self.convert.generate_token('admin'))
 
     def test_access_data(self):
-        self.assertEqual('You are under protected data', self.validate.access_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI'))
+        self.assertEqual('You are under protected data', self.validate.access_data('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4ifQ.BmcZ8aB5j8wLSK8CqdDwkGxZfFwM1X1gfAIN7cXOx9w'))
+
+    def test_access_data_wrong_token(self):
+        self.assertEqual('Access Denied, Invalid Token', self.validate.access_data('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYWRtaW4ifQ.BmcZ8aB5j8wLSK8qdDwkGxZfFwM1X1gfAIN7cXOx9w'))
+
+    def test_access_data_empty_token(self):
+        self.assertEqual('Access Denied, Invalid Token', self.validate.access_data(''))
 
 if __name__ == '__main__':
     unittest.main()
